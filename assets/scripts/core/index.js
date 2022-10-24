6808de50d5440ca1e264c5d430b367dcdd096a81
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-10-24 21:41:21 +0500

    dynamic year

--- assets/scripts/core/index.js
+++ assets/scripts/core/index.js
@@ -16,3 +16,48 @@ const partnerSwiper = new Swiper(".partnerSwiper", {
     el: ".swiper-pagination",
   },
 });
+
+// Abstract class
+class Select {
+  selectorValue;
+  /**
+   * ID or classname of an element
+   * @param {string} selectorValue
+   */
+  constructor(selectorValue) {
+    this.selectorValue = selectorValue;
+  }
+  /**
+   *
+   * @returns HTMLElement
+   */
+  getElement() {
+    try {
+      return document.querySelector(this.selectorValue);
+    } catch {
+      throw new Error("Bad selector!");
+    }
+  }
+}
+
+// Year
+class AssignYear {
+  element;
+  /**
+   * @param {string} id
+   * ID of an element
+   */
+  constructor(id) {
+    this.element = new Select(id).getElement();
+  }
+  /**
+   * @returns `current year`
+   */
+  assign() {
+    const year = new Date().getFullYear();
+    this.element.innerText = year;
+    return year;
+  }
+}
+
+const displayedYear = new AssignYear("#year").assign();
